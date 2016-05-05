cf7a52c4e955bdd0c33b9c046d59241f66485388
asafetzion <etzion.asaf@gmail.com>
2016-05-05 23:30:55 +0300

    changed to shuffled map

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -4,42 +4,7 @@
 #include "MapReduceFramework.h"
 #include "MapManager.h"
 
-using namespace std;
 
-typedef map<k2Base* ,list<v2Base*>> ShuffledMap; // maybe typedef the list also
-
-class MapReduceFramework
-{
-public:
-    // singleton design pattern
-    static MapReduceFramework& getInstance();
-
-    void insertToShuffledMap(k2Base * k2, list<v2Base *> v2Lists);
-    ShuffledMap::const_iterator getIteratorShuffledMap();
-
-private:
-    // singleton design pattern
-    MapReduceFramework() {};
-    MapReduceFramework(MapReduceFramework const&);
-    void operator=(MapReduceFramework const&);
-
-//  the map that contains the output of the shuffle function.
-    ShuffledMap _shuffledMap;
-
-};
-
-MapReduceFramework &MapReduceFramework::getInstance() {
-    static MapReduceFramework instance;
-    return instance;
-}
-
-void MapReduceFramework::insertToShuffledMap(k2Base * k2, list<v2Base *> v2Lists) {
-    _shuffledMap[k2] = v2Lists;
-}
-
-ShuffledMap::const_iterator MapReduceFramework::getIteratorShuffledMap() {
-    return _shuffledMap.begin();
-}
 
 // todo sort the output data
 
@@ -47,18 +12,22 @@ ShuffledMap::const_iterator MapReduceFramework::getIteratorShuffledMap() {
 
 // todo error handling function
 
+typedef std::map<k2Base* ,std::list<v2Base*>> ShuffledMap;
 
 OUT_ITEMS_LIST runMapReduceFramework(MapReduceBase &mapReduce,
                                      IN_ITEMS_LIST &itemsList,
                                      int multiThreadLevel) {
-    // create a map manager instance
 
-    // call the start mapping phase function
+    //  the map that contains the output of the shuffle function.
+    ShuffledMap shuffledMap;
 
-    //
+    // get a map manager instance
 
+    // call the start mapping phase method
 
+    // get a reduce manager instance
 
+    // call the start reduce phase method
 
 
     return std::list<OUT_ITEM>();

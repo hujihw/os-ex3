09658fe9fe355155566d8a4363b3454efd37e2ec
Omri Kaplan <omrikap@gmail.com>
2016-05-05 21:37:42 +0300

    Manager base class added

--- Manager.h
+++ Manager.h
@@ -1,17 +1,44 @@
 // Manager.h
 
 #include <vector>
-#include <sys/types.h>
+#include <pthread.h>
+#include <map>
+//#include <bits/unordered_map.h>
 #include "MapReduceFramework.h"
 
 #ifndef EX3_MANAGER_H
 #define EX3_MANAGER_H
 
 
-virtual class Manager { // todo keep virtual?
-public:
-    std::vector<pthread_t> threads;
+typedef struct mutexContainer
+{
+    std::vector<k2Base, v2Base> threadContainer;
+    pthread_mutex_t threadMutex;
 
+    mutexContainer() : threadContainer(), threadMutex(PTHREAD_MUTEX_INITIALIZER)
+    {
+
+    }
+} mutexContainer;
+
+
+class Manager {
+public:
+    Manager(int multiThreadLevel);
+
+    virtual ~Manager() = delete;
+
+    void threadCreator(void *(*start_routine) (void *), std::map &threadsMap);
+
+private:
+
+    static int _multiThreadLevel;
+
+    pthread_mutex_t mapExecThreadMapMutex;
+
+    std::vector<pthread_t*> _threads;
+
+    std::map<pthread_t, mutexContainer*> _threadsMap;
 };
 
 

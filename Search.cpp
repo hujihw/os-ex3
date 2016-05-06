f028e3f123f170021c56b36fc41b2560c676cee3
etzion.asaf <etzion.asaf@gmail.com>
2016-05-06 19:45:35 +0300

    finished writing the framework

--- Search.cpp
+++ Search.cpp
@@ -8,46 +8,60 @@ using namespace std;
 
 #define SUBSTRING 1
 #define FIRST_DIR 2
+#define SEPARATOR " "
+#define IGNORE_FILE_1 "."
+#define IGNORE_FILE_2 ".."
+#define NO_ARGS 1
+#define MISS_USAGE_MSG "Usage: <substring to search> <folders, separated by space>"
 
 /******************************************************************************/
-/*                       Keys and Values Decleration                          */
+/*                       Keys and Values Declaration                          */
 /******************************************************************************/
+
 
 class substringToSearch : public k1Base {
 public:
-    std::string substring;
-    substringToSearch(string givenSubstring)
-    {
+
+    // The input substring
+    string substring;
+
+    substringToSearch(string givenSubstring) {
         substring = givenSubstring;
-    };
-    bool operator<(const k1Base &other) const
-    {
+    }
+
+    // Empty operator
+    bool operator<(const k1Base &other) const {
         (void)other;
         return true;
-    };
+    }
 };
 
 
 class fileNameV1 : public v1Base {
 public:
-    std::string fileName;
-    fileNameV1(string givenfileNameA)
-    {
-        fileName = givenfileNameA;
-    };
+
+    // File name from one of the input directories
+    string fileName;
+
+    fileNameV1(string newFileName) {
+        fileName = newFileName;
+    }
 };
 
 
 class occurrenceK2 : public k2Base {
 public:
-    bool subExists;
-    occurrenceK2(bool givenSubExists)
-    {
-        subExists = givenSubExists;
-    };
 
-    bool operator<(const k2Base &other) const
-    {
+    // Indicating if the substring was found in the file names
+    bool subExists;
+
+    occurrenceK2(bool subExistsFlag) {
+        subExists = subExistsFlag;
+    }
+
+    bool operator<(const k2Base &other) const {
+
+        // Down casting and using the bool operator
         occurrenceK2 * temp = (occurrenceK2 *) &other;
         occurrenceK2 * temp2 = (occurrenceK2 *) this;
 
@@ -58,13 +72,17 @@ public:
 
 class fileNameV2 : public v2Base {
 public:
-    std::string fileName;
+
+    // File name, kept after Map
+    string fileName;
+
+    // Key pointed for memory control
     k2Base * keyPtr;
-    fileNameV2(string givenfileNameB, k2Base *  key)
-    {
-        fileName = givenfileNameB;
+
+    fileNameV2(string newFileName, k2Base * key) {
+        fileName = newFileName;
         keyPtr = key;
-    };
+    }
 
     ~fileNameV2(){
         delete(keyPtr);
@@ -74,16 +92,20 @@ public:
 
 class occurrenceK3 : public k3Base {
 public:
-    bool subExists;
-    occurrenceK3(bool givenSubExists)
-    {
-        subExists = givenSubExists;
-    };
 
-    bool operator<(const k3Base &other) const
-    {
+    // Indicating if the substring was found in the file names
+    bool subExists;
+
+    occurrenceK3(bool subExistsFlag) {
+        subExists = subExistsFlag;
+    }
+
+    bool operator<(const k3Base &other) const {
+
+        // Down casting and using the bool operator
         occurrenceK3 * temp = (occurrenceK3 *) & other;
         occurrenceK3 * temp2 = (occurrenceK3 *) this;
+
         return (temp->subExists < temp2->subExists);
     }
 };
@@ -91,79 +113,115 @@ public:
 
 class fileNameList : public v3Base {
 public:
-    V2_LIST fileNamesLst;
-    fileNameList(const V2_LIST* givenfileNames)
-    {
-        fileNamesLst = *givenfileNames;
-    };
+
+    // File names that exists or don't
+    V2_LIST fileNamesList;
+
+    fileNameList(const V2_LIST* givenNames) {
+        fileNamesList = *givenNames;
+    }
 
     ~fileNameList(){
-        for(auto v2Itm : fileNamesLst){
+
+        // Deleting all V2 in the list
+        for(auto v2Itm : fileNamesList){
             delete(v2Itm);
         }
-    };
+    }
 };
 
 
-class findFiles : public MapReduceBase {
+/******************************************************************************/
+/*                     Search Map and Reduce Functions                        */
+/******************************************************************************/
+
+class SearchFunctions : public MapReduceBase {
 
     void Map(const k1Base *const key, const v1Base *const val) const{
 
+
+
+        // Down casting the key and value
         string substring = ((substringToSearch *) key) ->substring;
         string filename = ((fileNameV1 *) val) -> fileName;
 
-        bool isSub = false;
+//        std::cout<<substring<<std::endl; // todo remove
+//        std::cout<<filename<<std::endl; // todo remove
 
+
+        // Checking if the string is a substring of the file name
+        bool isSub = false;
         if (filename.find(substring) != string::npos){
             isSub = true;
         }
-        occurrenceK2 * occurance = new occurrenceK2(isSub);
-        fileNameV2 * newFilename = new fileNameV2(filename, occurance);
 
-        Emit2(occurance, newFilename);
+        // Creating the appropriate K2 V2, and calling Emit 2
+        occurrenceK2 * occurrence = new occurrenceK2(isSub);
+        fileNameV2 * newFilename = new fileNameV2(filename, occurrence);
+
+        Emit2(occurrence, newFilename);
     };
 
 
     void Reduce(const k2Base *const key, const V2_LIST &vals) const{
 
+        // Down casting the key and value
         bool keyVal = ((occurrenceK2 *)key)->subExists;
 
-        occurrenceK3 * occurance = new occurrenceK3(keyVal);
+//        std::cout<<"key val:" <<std::endl; // todo remove
+
+
+        // Creating the appropriate K3 V3, and calling Emit 3
+        occurrenceK3 * occurrence = new occurrenceK3(keyVal);
         fileNameList * newFilenameList = new fileNameList(&vals);
 
-        Emit3(occurance, newFilenameList);
+        Emit3(occurrence, newFilenameList);
     };
 };
 
 
+/******************************************************************************/
+/*                           Search Main Function                             */
+/******************************************************************************/
 
 int main(int argc,char *argv[]){
 
-    string dirName;
-    string file;
-    IN_ITEMS_LIST inItems;
+    // If no arguments are given
+    if(argc == NO_ARGS){
+        cerr << MISS_USAGE_MSG << endl;
+        exit(0);
+    }
 
+    string directory;
+    string file;
     DIR *dp;
+
     struct dirent *dirp;
+    bool notEmpty = false;
+
+    IN_ITEMS_LIST inputItems;
+
 
     // Creating the input list
     substringToSearch * subStringK1 = new substringToSearch(argv[SUBSTRING]);
 
     for (int i = FIRST_DIR; i < argc; i++) {
-        dirName = argv[i];
 
-        if ((dp = opendir(dirName.c_str())) == NULL) {
-            // TODO
+        directory = argv[i];
+        if ((dp = opendir(directory.c_str())) == NULL) {
+            continue;
         }
 
+        // Reading files from the directory
         while ((dirp = readdir(dp)) != NULL) {
 
             file = string(dirp->d_name);
 
-            if ((strcmp(file.c_str(), ".") != 0) &&
-                (strcmp(file.c_str(), "..") != 0)) {
+            if ((strcmp(file.c_str(), IGNORE_FILE_1) != 0) &&
+                (strcmp(file.c_str(), IGNORE_FILE_2) != 0)) {
+
                 fileNameV1 *fileName = new fileNameV1(file);
-                inItems.push_back(make_pair(subStringK1, fileName));
+                inputItems.push_back(make_pair(subStringK1, fileName));
             }
 
         }
@@ -171,26 +229,38 @@ int main(int argc,char *argv[]){
     }
 
     OUT_ITEMS_LIST result;
-    findFiles ff;
+    SearchFunctions searchFunctions;
 
-    result = runMapReduceFramework(ff, inItems, 1);
+    result = runMapReduceFramework(searchFunctions, inputItems, 5);
 
+
+    // Printing results
     for(auto resPair : result){
+
+        // Printing only files that include the substring
         if(((occurrenceK2 *)resPair.first)->subExists){
 
-            V2_LIST resList = ((fileNameList *) resPair.second)->fileNamesLst;
+            V2_LIST resList = ((fileNameList *) resPair.second)->fileNamesList;
 
             for(auto resItem : resList){
+                notEmpty = true;
                 string toPrint = ((fileNameV2 *) resItem)->fileName;
-                cout << toPrint << endl;
+                cout << toPrint <<  SEPARATOR;
             }
         }
     }
 
+    // If any files are printed, line down
+    if(notEmpty){
+        cout << endl;
+    }
 
-    for(auto inPair : inItems){
+
+    // Freeing memory
+    for(auto inPair : inputItems){
         delete(inPair.second);
     }
+
     delete(subStringK1);
 
     for(auto outPair : result){
@@ -198,7 +268,3 @@ int main(int argc,char *argv[]){
         delete(outPair.second);
     }
 }
-
-
-
-
